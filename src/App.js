cc6cca1ec82e5a863d22383bdf5ecfc89f28eda2
Giorgio Zamparelli <giorgio.zamparelli@gmail.com>
2023-01-18 10:51:29 +0100

    feat: translate Learn React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
+import { i18n } from '@lingui/core'
+import { I18nProvider } from '@lingui/react'
+import { Trans } from '@lingui/macro'
+import { messages as enMessages } from './locales/en/messages'
+import { messages as esMessages } from './locales/es/messages'
 import logo from './logo.svg';
 import './App.css';
 
+i18n.load({
+  en: enMessages,
+  es: esMessages,
+})
+i18n.activate('es')
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <I18nProvider i18n={i18n}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Trans>Learn React</Trans>
+          </a>
+        </header>
+      </div>
+    </I18nProvider>
   );
 }
 
